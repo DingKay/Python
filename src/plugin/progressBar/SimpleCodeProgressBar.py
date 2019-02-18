4e4b723de8d104758fb345db32defca70f1ca9ff
DingKay <pigsties@outlook.com>
2019-02-18 15:49:45 +0800

    Simple Code Progress Bar

---
+++ src/plugin/progressBar/SimpleCodeProgressBar.py
@@ -0,0 +1,10 @@
+import sys
+import time
+
+print("#### START ####")
+for i in range(100):
+    sys.stdout.write('\r')
+    sys.stdout.write("[%s>%s] %0.2f" % ('-'*i, ' '*(100-i), float(i)/float(100)))
+    time.sleep(1)
+print("")
+print("#### END ####")
