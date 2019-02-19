43eae39a463ea2901ea1830191cc2de11ca21399
DingKay <pigsties@outlook.com>
2019-02-19 16:48:19 +0800

    modify time

--- src/plugin/progressBar/SimpleCodeProgressBar.py
+++ src/plugin/progressBar/SimpleCodeProgressBar.py
@@ -5,6 +5,6 @@ print("#### START ####")
 for i in range(100):
     sys.stdout.write('\r')
     sys.stdout.write("[%s>%s] %0.2f" % ('-'*i, ' '*(100-i), float(i)/float(100)))
-    time.sleep(1)
+    time.sleep(0.1)
 print("")
 print("#### END ####")
